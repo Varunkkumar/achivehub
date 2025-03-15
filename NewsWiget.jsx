b9fb9d9d2cdb84bb42045d050ccafbf3d7c7def2
Varun.k.kumar <varunkausal2004@gmail.com>
2025-03-15 17:36:15 +0530

    Create NewsWiget.jsx

---
+++ NewsWiget.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const NewsWidget = () => {
+  return (
+    <div className="news-widget">
+      <h4>LinkedIn News</h4>
+      <ul>
+        <li>Tech sector grows 15%</li>
+        <li>Remote work trends</li>
+        <li>New React features</li>
+      </ul>
+    </div>
+  );
+};
+
+export default NewsWidget;
