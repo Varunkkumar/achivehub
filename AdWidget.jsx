e83201e88ce7ad409d13b78d96a9510151d4cfff
Varun.k.kumar <varunkausal2004@gmail.com>
2025-03-15 16:44:23 +0530

    Create AdWidget.jsx

---
+++ AdWidget.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AdWidget = () => {
+  return (
+    <div className="ad-widget">
+      <h4>Advertisement</h4>
+      <p>Enhance your career with our courses</p>
+    </div>
+  );
+};
+
+export default AdWidget;
